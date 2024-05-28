86ac7534d546bc2766c8c96e28037a563b11ed05
owner <milota@mindgrip.com>
2024-05-28 16:18:14 -0700

    did a little cleanup

--- main.py
+++ main.py
@@ -1,77 +1,6 @@
-
-import random
 import os
 import GUI as gui
 
-# gestures = ""
-# # the GUI stuff --------------------------------------------------------------------------
-# def change_color(button):
-#     # Generate random RGB color values
-#     r = random.randint(0, 255)
-#     g = random.randint(0, 255)
-#     b = random.randint(0, 255)
-#
-#     # Convert RGB values to hexadecimal color code
-#     color = '#{:02x}{:02x}{:02x}'.format(r, g, b)
-#
-#     # Change the background color of the button
-#     button.config(bg=color)
-# def run_callback():
-#     global gestures
-#     # get the text from the text entry box
-#     command = text_entry.get("1.0", "end-1c")
-#     print("command", command)
-#     if gestures != "":
-#         command += ". The user clicked on " + gestures
-#     r = agent_executor.invoke(
-#         {
-#             "input": command,
-#         }
-#     )
-#     gestures = ""
-#     print(r)
-#
-#
-# def button1_clicked():
-#     # Placeholder function for the first button click event
-#     global gestures
-#     gestures = "button 1"
-#     print("Button 1 clicked")
-#
-# def button2_clicked():
-#     # Placeholder function for the second button click event
-#     global gestures
-#     gestures = "button 2"
-#     print("Button 2 clicked")
-
-# Create main window
-# root = tk.Tk()
-# root.title("GUI Example")
-
-# Create text entry box
-# text_entry = tk.Text(root, width=50, height=20)
-# text_entry.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
-#
-# # Create Button 1
-# button1 = tk.Button(root, text="Button 1", command=button1_clicked)
-# button1.grid(row=0, column=1, padx=(0, 10), pady=10, sticky="ne")
-#
-# # Create Button 2
-# button2 = tk.Button(root, text="Button 2", command=button2_clicked)
-# button2.grid(row=0, column=1, padx=(0, 10), pady=40, sticky="ne")
-#
-# # Create "Run" button
-# run_button = tk.Button(root, text="Run", command=run_callback)
-# run_button.grid(row=2, column=0, columnspan=2, padx=10, pady=(0, 10), sticky="ew")
-#
-# # Configure grid row and column weights
-# root.grid_rowconfigure(0, weight=1)
-# root.grid_rowconfigure(1, weight=1)
-# root.grid_rowconfigure(2, weight=0)
-# root.grid_columnconfigure(0, weight=1)
-# root.grid_columnconfigure(1, weight=0)
-#
-# print("the GUI is up and running")
 
 # load the lange chain stuff
 open_ai_key = os.environ.get('OPENAI_API_KEY')
@@ -100,8 +29,6 @@ from langchain.prompts import PromptTemplate
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-
-
 
 # Get the prompt to use - can be replaced with any prompt that includes variables "agent_scratchpad" and "input"!
 prompt = hub.pull("hwchase17/openai-tools-agent")
@@ -147,5 +74,3 @@ agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 window.set_agent_executor(agent_executor)
 window.run()
-
-
